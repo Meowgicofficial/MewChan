8327b5e3fe5976e356fb87bda0e98c82b18e6238
Meowgic <ggmyxd222@gmail.com>
2026-03-29 12:58:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  key: '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'
+};
+```
+
+---
+
+## ขั้นที่ 2 — สร้าง `.gitignore`
+สร้างไฟล์ใหม่ชื่อ `.gitignore`:
+```
+config.js
